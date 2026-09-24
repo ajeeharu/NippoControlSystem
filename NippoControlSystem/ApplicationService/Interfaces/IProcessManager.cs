7208ba752d5922b49148eb4a461295e64a5b877d
清水南公民館 <91398263+ajeeharu@users.noreply.github.com>
2026-09-24 20:27:06 +0900

    Win32APIをリファクタリング

---
+++ NippoControlSystem/ApplicationService/Interfaces/IProcessManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace NippoControlSystem.ApplicationService.Interfaces
+{
+    public interface IProcessManager
+    {
+        /// <summary>
+        /// 実行中の同じアプリケーションのプロセスを取得します。
+        /// </summary>
+        Process? GetPreviousProcess();
+
+        /// <summary>
+        /// 指定されたプロセスのウィンドウを検索し、最前面に表示します。
+        /// </summary>
+        /// <param name="target">対象プロセス</param>
+        /// <param name="caption">対象ウィンドウのタイトル</param>
+        void WakeupWindow(Process target, string caption);
+    }
+}
---
+++ NippoControlSystem/ApplicationService/Interfaces/enums/IProcessManager.cs
@@ -0,0 +1,21 @@
+﻿namespace NippoControlSystem.ApplicationService.Interfaces.enums
+{
+    /// <summary>
+    /// ShowWindowAsync 関数で使用する表示コマンド
+    /// </summary>
+    public enum ShowWindowCommand : int
+    {
+        SW_HIDE = 0,
+        SW_NORMAL = 1,
+        SW_SHOWMINIMIZED = 2,
+        SW_MAXIMIZE = 3,
+        SW_SHOWNOACTIVATE = 4,
+        SW_SHOW = 5,
+        SW_MINIMIZE = 6,
+        SW_SHOWMINNOACTIVE = 7,
+        SW_SHOWNA = 8,
+        SW_RESTORE = 9,
+        SW_SHOWDEFAULT = 10,
+        SW_MAX = 11
+    }
+}
